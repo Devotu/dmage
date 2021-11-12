fc11c3c0ea8ceb51afe3e84d0274ef143e554242
Ottu <otuvesson@gmail.com>
2021-11-12 21:22:03 +0100

    Calc - Damage

--- lib/dmage_web/live/main_live.ex
+++ lib/dmage_web/live/main_live.ex
@@ -1,6 +1,8 @@
 defmodule DmageWeb.MainLive do
   use DmageWeb, :live_view
 
+  @faces 6
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, attack: 0, results: [])}
@@ -13,7 +15,18 @@ defmodule DmageWeb.MainLive do
       {false, _inputs} ->
         {:noreply, put_flash(socket, :feedback, "Invalid input")}
       _ ->
-        {:noreply, assign(socket, results: [inputs |> calculate] ++ socket.assigns.results)}
+        pretty_input = inputs |> inputs_to_numbers()
+        open_normal_damage = pretty_input |> calculate_normal_in_open()
+        open_crit_damage = pretty_input |> calculate_crit_in_open()
+        result = %{
+          input: pretty_input,
+          open: %{
+            normal_damage: open_normal_damage,
+            crit_damage: open_crit_damage,
+            total_damage: open_normal_damage + open_crit_damage,
+          },
+        }
+        {:noreply, assign(socket, results: [Kernel.inspect(result)] ++ socket.assigns.results)}
     end
   end
 
@@ -26,8 +39,7 @@ defmodule DmageWeb.MainLive do
 
   ## Helpers
   defp inputs_are_numbers({true, inputs}) do
-    valid = inputs
-    |> Enum.map(&Integer.parse/1)
+    valid = inputs_to_numbers(inputs)
     |> Enum.member?(:error)
     |> then(&(!&1))
 
@@ -35,9 +47,7 @@ defmodule DmageWeb.MainLive do
   end
 
   defp inputs_are_in_range({true, inputs}) do
-    valid = inputs
-    |> Enum.map(&Integer.parse/1)
-    |> Enum.map(fn {n, ""} -> n end)
+    valid = inputs_to_numbers(inputs)
     |> Enum.min()
     |> then(&(&1 > 0))
 
@@ -45,14 +55,23 @@ defmodule DmageWeb.MainLive do
   end
   defp inputs_are_in_range({false, _inputs} = error), do: error
 
-  defp calculate(inputs) do
+  defp calculate_normal_in_open([attack_dice, skill, normal_damage, _crit_damage]) do
+    hit_eyes = @faces - skill
+    calculate_damage(attack_dice, hit_eyes, normal_damage)
+  end
+
+  defp calculate_crit_in_open([attack_dice, _skill, _normal_damage, crit_damage]) do
+    calculate_damage(attack_dice, 1, crit_damage)
+  end
+
+  defp calculate_damage(attack_dice, hit_eyes, damage) do
+    hits = attack_dice * (hit_eyes / @faces)
+    hits * damage
+  end
+
+  defp inputs_to_numbers(inputs) do
     inputs
     |> Enum.map(&Integer.parse/1)
     |> Enum.map(fn {n, ""} -> n end)
-    |> calculate_in_open()
-  end
-
-  defp calculate_in_open([a, s, n, c]) do
-    a + s + n + c
   end
 end
