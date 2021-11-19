fcb4d00263c2e167e94625ae3a2b933c54909bef
Ottu <otuvesson@gmail.com>
2021-11-19 23:03:26 +0100

    Calc - Resolve probable

--- lib/dmage/range.ex
+++ lib/dmage/range.ex
@@ -1,6 +1,5 @@
 defmodule Dmage.Calculator.Range do
   @faces 6
-  @defence 3
 
   def hits(dice, _eyes) when dice < 0, do: error "dice cannot be negative"
   def hits(_dice, eyes) when eyes < 0, do: error "eyes cannot be negative"
@@ -28,32 +27,16 @@ defmodule Dmage.Calculator.Range do
     {normal, crit}
   end
 
+  def resolve({hits_normal, hits_crit}, {saves_normal, saves_crit}, {damage_normal, damage_crit}) do
+    damage_normal = damage(hits_normal - saves_normal, damage_normal)
+    damage_crit = damage(hits_crit - saves_crit, damage_crit)
+    {damage_normal, damage_crit}
+  end
 
-
-  def damage(dice, _eyes, _damage) when dice < 0, do: error "dice cannot be negative"
-  def damage(_dice, eyes, _damage) when eyes < 0, do: error "eyes cannot be negative"
-  def damage(_dice, eyes, _damage) when eyes > @faces, do: error "eyes cannot excced #{@faces}"
-  def damage(_dice, _eyes, damage) when damage < 0, do: error "damage cannot be negative"
-  def damage(attack_dice, hit_eyes, damage) do
-    hits = attack_dice * (hit_eyes / @faces)
+  def damage(hits, damage) do
     hits * damage
     |> Float.round(2)
   end
-
-  def damage(dice, _skill, _damage_normal, _damage_crit) when dice < 0, do: error "dice cannot be negative"
-  def damage(_dice, skill, _damage_normal, _damage_crit) when skill < 1, do: error "skill cannot be less than 1"
-  def damage(_dice, skill, _damage_normal, _damage_crit) when skill > @faces, do: error "skill cannot excced #{@faces}"
-  def damage(_dice, _skill, damage_normal, _damage_crit) when damage_normal < 0, do: error "damage cannot be negative"
-  def damage(_dice, _skill, _damage_normal, damage_crit) when damage_crit < 0, do: error "damage cannot be negative"
-  def damage(attack_dice, skill, damage_normal, damage_crit) do
-    normal = damage(attack_dice, @faces - skill, damage_normal)
-    crit = damage(attack_dice, 1, damage_crit)
-    {normal, crit}
-  end
-
-
-
-
 
   defp error(msg) do
     {:error, msg}
--- test/dmage/calculator_range_test.exs
+++ test/dmage/calculator_range_test.exs
@@ -27,6 +27,9 @@ defmodule Dmage.CalculatorRangeTest do
     assert {1/2, 1/6} == Range.attacks(1, 3)
     assert {1/3, 1/6} == Range.attacks(1, 4)
 
+    assert {2.0, 2/3} == Range.attacks(4, 3)
+    assert {4/3, 2/3} == Range.attacks(4, 4)
+
     #min/max
     assert {0.0, 0.0} == Range.attacks(0, 6)
     assert {0.0, 1.0} == Range.attacks(6, 6)
@@ -62,93 +65,7 @@ defmodule Dmage.CalculatorRangeTest do
     assert {:error, "save cannot be greater than 6"} == Range.saves(dice, 7)
   end
 
-  test "calculate damage" do
-    #normal
-    assert 3.0 = Range.damage(3, 2, 3)
-    assert 4.5 = Range.damage(3, 3, 3)
-    assert 6.0 = Range.damage(3, 4, 3)
-
-    #min
-    assert 0.0 = Range.damage(0, 3, 4)
-    assert 0.0 = Range.damage(0, 6, 4)
-
-    assert 0.0 = Range.damage(3, 0, 4)
-    assert 0.0 = Range.damage(6, 0, 4)
-
-    assert 0.0 = Range.damage(6, 3, 0)
-    assert 0.0 = Range.damage(3, 6, 0)
-
-    #illegal
-    assert {:error, "dice cannot be negative"} == Range.damage(-1, 0, 0)
-    assert {:error, "eyes cannot be negative"} == Range.damage(0, -1, 0)
-    assert {:error, "eyes cannot excced 6"} == Range.damage(0, 7, 0)
-    assert {:error, "damage cannot be negative"} == Range.damage(0, 0, -1)
-  end
-
-  test "calculate total damage" do
-    #normal
-    assert {3.0, 2.0} == Range.damage(3, 4, 3, 4)
-    assert {4.5, 1.5} == Range.damage(3, 3, 3, 3)
-    assert {5.33, 2.67} == Range.damage(4, 4, 4, 4)
-    assert {10.0, 6.67} == Range.damage(10, 4, 3, 4)
-    assert {5.0, 3.0} == Range.damage(3, 4, 5, 6)
-    assert {4.0, 1.5} == Range.damage(3, 2, 2, 3)
-
-    #min
-    assert {0.0, 0.0} == Range.damage(0, 4, 3, 4)
-    assert {0.0, 2.0} == Range.damage(3, 6, 3, 4)
-
-    #max
-    assert {7.5, 2.0} == Range.damage(3, 1, 3, 4)
-    assert {7.5, 0.0} == Range.damage(3, 1, 3, 0)
-    assert {0.0, 2.0} == Range.damage(3, 1, 0, 4)
-
-    #fractal
-    assert {3.75, 1.67} == Range.damage(2.5, 3, 3, 4)
-    assert {1.5, 0.67} == Range.damage(1.0, 3, 3, 4)
-    assert {0.75, 0.33} == Range.damage(0.5, 3, 3, 4)
-
-    #illegal
-    assert {:error, "dice cannot be negative"} == Range.damage(-1, 0, 0, 0)
-    assert {:error, "skill cannot be less than 1"} == Range.damage(0, 0, 0, 0)
-    assert {:error, "skill cannot excced 6"} == Range.damage(0, 7, 0, 0)
-    assert {:error, "damage cannot be negative"} == Range.damage(0, 1, -1, 0)
-    assert {:error, "damage cannot be negative"} == Range.damage(0, 1, 0, -1)
-  end
-
-  test "calculate saves" do
-    dice = 3
-
-    #normal
-    assert {0.5, 0.5} == Range.saves(dice, 5)
-    assert {1.0, 0.5} == Range.saves(dice, 4)
-    assert {1.5, 0.5} == Range.saves(dice, 3)
-
-    #min/max
-    assert {2.5, 0.5} == Range.saves(dice, 1)
-    assert {0.0, 0.5} == Range.saves(dice, 6)
-
-    #variation
-    assert {2/3, 1/3} == Range.saves(2, 4)
-    assert {1/3, 1/6} == Range.saves(1, 4)
-    assert {0.0, 0.0} == Range.saves(0, 4)
-
-    #illegal
-    assert {:error, "dice cannot be negative"} == Range.saves(-1, 6)
-    assert {:error, "save cannot be less than 1"} == Range.saves(dice, 0)
-    assert {:error, "save cannot be greater than 6"} == Range.saves(dice, 7)
-  end
-
-  test "calculate open" do
-    #even
-    assert {0.0, 0.0} == Range.in_open([3, 4, 3, 4, 4])
-
-    #damage
-    assert {3.0, 4.0} == Range.in_open([3, 3, 3, 4, 5])
-
-    #oversave
-    assert {0.0, 0.0} == Range.in_open([3, 5, 3, 4, 3])
-
-    #illegal
+  test "resolve probable" do
+    assert {3.0, 0.67} == Range.resolve({2.0, 2/3}, {1.0, 0.5}, {3, 4})
   end
 end
